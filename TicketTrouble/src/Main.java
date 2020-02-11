989dd1477fe54d5ec6122755405c69fbc2193c53
mlmosteiro <marymost22@gmail.com>
2020-02-12 00:27:48 +0100

    2019:1 - Ticket trouble

---
+++ TicketTrouble/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            new Solution().main(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
